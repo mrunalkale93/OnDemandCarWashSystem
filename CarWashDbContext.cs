32e42f2fa11f6c6fc4d0969474d317f52b9ba7b2
mrunalkale93 <123743586+mrunalkale93@users.noreply.github.com>
2023-05-16 01:40:12 +0530

    addDbContext

---
+++ CarWashDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CarWashSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CarWashSystem.Data
+{
+    public class CarWashDbContext:DbContext
+    {
+        public CarWashDbContext(DbContextOptions dbcontextOptions) : base(dbcontextOptions) { }
+
+        public DbSet<AddOn> AddOns { set; get; }
+        public DbSet<Car> Cars { set; get; }
+        public DbSet<OrderDetails> Orders { set; get; }
+        public DbSet<Payment> Payments { set; get; }
+
+        public DbSet<WashPackageDetails> WashPackages { set; get; }
+        public DbSet<UserDetails> Users { set; get; }
+    }
+}
