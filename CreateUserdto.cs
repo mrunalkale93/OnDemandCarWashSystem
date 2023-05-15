98e7b6ebb2fab76c093bf2eb4bc4ce4d744d1c5d
mrunalkale93 <123743586+mrunalkale93@users.noreply.github.com>
2023-05-16 01:42:57 +0530

    Add DTO

---
+++ CreateUserdto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CarWashSystem.DTO
+{
+    public class CreateUserdto
+    {
+        [Required(ErrorMessage = "Full Name is required")]
+        [DisplayName("Full Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+
+
+        public string Password { get; set; }
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Role is required")]
+        public string Role { get; set; }
+    }
+}
---
+++ UserUpdatedto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarWashSystem.DTO
+{
+    public class UserUpdatedto
+    {
+        [Required(ErrorMessage = " Name is required")]
+        [DisplayName(" Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+
+        public string Password { get; set; }
+        public string Address { get; set; }
+    }
+}
