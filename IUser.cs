c8769f27be31b103332f5a30bacc5563f84ae77e
mrunalkale93 <123743586+mrunalkale93@users.noreply.github.com>
2023-05-16 01:45:00 +0530

    Add Interface

---
+++ IUser.cs
@@ -0,0 +1,13 @@
+﻿using CarWashSystem.Models;
+
+namespace CarWashSystem.Interface
+{
+    public interface IUser
+    {
+        Task<List<UserDetails>> GetUsers();
+        Task<UserDetails> GetUserById(int id);
+        Task<UserDetails> CreateUser(UserDetails user);
+        Task<UserDetails> UpdateUser(int id, UserDetails user);
+        Task<UserDetails> DeleteUser(int id);
+    }
+}
---
+++ IWashPackage.cs
@@ -0,0 +1,13 @@
+﻿using CarWashSystem.Models;
+
+namespace CarWashSystem.Interface
+{
+    public interface IWashPackage
+    {
+        Task<List<WashPackageDetails>> GetAllWashPackage();
+        Task<WashPackageDetails> GetWashPackageById(int id);
+        Task<WashPackageDetails> CreateWashPackage(WashPackageDetails washPackage);
+        Task<WashPackageDetails> UpdateWashPackage(int id, WashPackageDetails washPackage);
+        Task<WashPackageDetails> DeleteWashPackage(int id);
+    }
+}
